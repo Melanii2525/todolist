db5631fc3d01584adcf4f5c36d87c760785c79d7
Melanii2525 <melaniazzuranajma@gmail.com>
2025-02-05 19:58:53 +0700

    Add files via upload

---
+++ src/arrow-function.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const TodoApp = ()=>{
+    return(
+        <div className="container">
+        <section id="judul">
+          <p className="judul text-center fs-3 fw-bold mt-3"
+          // style={{background: "yellow", border: "1px solid red"}}
+          >My To Do List</p>
+        </section>
+
+        <section id="input">
+          <div className="row justify-content-center align-item-center mb-3">
+            <div className="col-8">
+              <input type="text" className="fore-control" placeholder='Masukkan Tugas Baru'/>
+            </div>
+            <div className="col-auto">
+              <button className="btn btn-primary">Tambah</button>
+            </div>
+          </div>
+        </section>
+
+        <section id="ist"></section>
+        <section id="hide"></section>
+      </div>
+    )
+}
+
+export default TodoApp;
